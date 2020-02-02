97197ffc5e0060787278bbe68375d7255c72e8e7
seunghaekim <aka.seunghaekim@gmail.com>
2020-02-02 23:29:12 +0900

    ADD analytic code

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,18 +1,18 @@
 export default {
   srcDir: 'src/',
-  // buildModules: [
-  //   ['@nuxtjs/google-analytics', {
-  //     id: 'UA-78070436-7'
-  //   }]
-  // ],
+  buildModules: [
+    ['@nuxtjs/google-analytics', {
+      id: 'UA-78070436-3 '
+    }]
+  ],
   css: [
     '~assets/css/reset.css'
   ],
   head: {
     meta: [
       { charset: 'utf-8' },
-      { hid: 'description', name: 'description', content: 'javascript port of automatic_insurrection' },
-      { title: 'Automatic Insurrection js'}
+      { hid: 'description', name: 'description', content: 'Oblique Strategies Prompter' },
+      { title: 'Oblique Strategies Prompter'}
     ],
     link: [
       {rel: 'stylesheet', src: 'https://fonts.googleapis.com/css?family=Libre+Baskerville',},
