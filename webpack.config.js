2f42506215366c66d79cb496587746b6c30ee23f
seunghaekim <aka.seunghaekim@gmail.com>
2020-02-07 00:10:23 +0900

    MOVE index.html to src as template

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,20 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
+  mode: 'production',
   entry: './src/script.js',
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'main.js'
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.html',
+      title: 'The Oblique Strategies Prompter'
+    })
+  ],
   module: {
     rules: [
       {
